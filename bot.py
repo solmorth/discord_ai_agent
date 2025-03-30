1f7a1a24cfa808f5048fcdf0fc0a98922b4eecb6
solmorth <solmorth@gmail.com>
2025-03-31 01:05:11 +0200

    Code formatting

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ import os
 from logging import getLogger
 from message_handler import Handler
 
-logging = getLogger('discord.agent')
+logging = getLogger("discord.agent")
 
 
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-N8N_WEBHOOK_URL = os.getenv('N8N_WEBHOOK_URL')
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL")
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -19,23 +19,26 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 message_handler = Handler(client=client, webhook_url=N8N_WEBHOOK_URL)
 
+
 @client.event
 async def on_ready():
-      logging.info(
-      'Ready! Invite URL: %s',
-      discord.utils.oauth_url(
-        client.application_id,
-        permissions=discord.Permissions(
-          read_messages=True,
-          send_messages=True,
-          create_public_threads=True,
+    logging.info(
+        "Ready! Invite URL: %s",
+        discord.utils.oauth_url(
+            client.application_id,
+            permissions=discord.Permissions(
+                read_messages=True,
+                send_messages=True,
+                create_public_threads=True,
+            ),
+            scopes=["bot"],
         ),
-        scopes=['bot'],
-      ),
     )
+
 
 @client.event
 async def on_message(message):
-  await message_handler.handle_message(message)
+    await message_handler.handle_message(message)
+
 
 client.run(TOKEN)
